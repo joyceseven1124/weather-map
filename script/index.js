ab602b00d48be31c502bd61da8512e0b4c0b4984
jamieyu0914 <jamieyu0914@gmail.com>
2023-01-06 17:25:24 +0800

    [Jamie]-17th Commit-feat now_clock

--- script/index.js
+++ script/index.js
@@ -94,9 +94,73 @@ now_Date = new Date(now_Date.getTime() - offset * 60 * 1000);
 day = now_Date.toISOString().split(".")[0];
 today_now_date = now_Date.toISOString().split(".")[0].split("T")[0];
 today_now_time = now_Date.toISOString().split(".")[0].split("T")[1];
-console.log(day);
-console.log(today_now_date);
-console.log(today_now_time);
+
+// for (var i = 0; i < 5; i++) {
+//   window.setTimeout(function () {
+//     updatetime();
+//   }, 1000);
+// }
+
+var startTime = new Date().getTime();
+var second = Number(today_now_time.split(":")[2]);
+
+// 用來模擬延遲 Queue 的動作
+window.setInterval(function () {
+  var i = 0;
+  while (i++ < 999999) {}
+}, 0);
+
+window.setInterval(function () {
+  var endTime = new Date().getTime();
+  if (second == 60) {
+    second = 0;
+  }
+  second++;
+  updatetime(second);
+}, 1000);
+
+function updatetime(second) {
+  if (second == 60) {
+    second = "00";
+  } else if (second == 1) {
+    second = "01";
+  } else if (second == 2) {
+    second = "02";
+  } else if (second == 3) {
+    second = "03";
+  } else if (second == 4) {
+    second = "04";
+  } else if (second == 5) {
+    second = "05";
+  } else if (second == 6) {
+    second = "06";
+  } else if (second == 7) {
+    second = "07";
+  } else if (second == 8) {
+    second = "08";
+  } else if (second == 9) {
+    second = "09";
+  }
+
+  our_today_now_date =
+    today_now_date.split("-")[0] +
+    "/" +
+    today_now_date.split("-")[1] +
+    "/" +
+    today_now_date.split("-")[2]; //今日日期
+  our_today_now_time =
+    today_now_time.split(":")[0] +
+    ":" +
+    today_now_time.split(":")[1] +
+    ":" +
+    second;
+
+  now_clock = our_today_now_date + " " + our_today_now_time; //現在時刻
+
+  document.querySelector(".time").innerHTML = now_clock;
+
+  // const document.qu
+}
 
 async function fetchWeather(region, day) {
   let url = `https://opendata.cwb.gov.tw/api/v1/rest/datastore/F-C0032-001?Authorization=CWB-480FD02B-2155-49AB-B24B-1796E44F6333&locationName=${region}`;
@@ -174,32 +238,27 @@ async function fetchWeather(region, day) {
   if (weatherTypes["isClear"].includes(weatherNumber)) {
     overlayEl.style.backgroundColor = "var(--isClear)";
     weatherIcon.src = "./assets/isClear.png";
-
   } else if (weatherTypes["isOvercast"].includes(weatherNumber)) {
     overlayEl.style.backgroundColor = "var(--isOvercast)";
     weatherIcon.src = "./assets/isOvercast.png";
     weatherPlace.style.bottom = "60px";
-
   } else if (weatherTypes["isThunderstorm"].includes(weatherNumber)) {
     overlayEl.style.backgroundColor = "var(--isThunderstorm)";
     weatherIcon.src = "./assets/isThunderstorm.png";
     weatherPlace.style.bottom = "10px";
-
   } else if (weatherTypes["isRain"].includes(weatherNumber)) {
     overlayEl.style.backgroundColor = "var(--isRain)";
     weatherIcon.src = "./assets/isRain.png";
     weatherPlace.style.bottom = "40px";
-
   } else if (weatherTypes["isSnowing"].includes(weatherNumber)) {
     overlayEl.style.backgroundColor = "var(--isSnowing)";
     weatherIcon.src = "./assets/isSnowing.png";
     weatherPlace.style.bottom = "20px";
-
   } else if (weatherTypes["isSunnywithCloudy"].includes(weatherNumber)) {
     overlayEl.style.backgroundColor = "var(--isSunnywithCloudy)";
     weatherIcon.src = "./assets/isSunnywithCloudy.png";
     weatherPlace.style.bottom = "60px";
-  };
+  }
   //測試區
   //overlayEl.style.backgroundColor = "yellow";
   //weatherIcon.src = "./assets/isSunnywithCloudy.png";
@@ -231,7 +290,7 @@ function gethelp() {
 
 document.querySelector("#close").addEventListener("click", () => {
   citySelectorContainer.style.display = "none";
-})
+});
 
 document.querySelector(".overlay").addEventListener("click", () => {
   if (modalEl.classList.value === "modal active") {
@@ -243,67 +302,63 @@ document.querySelector(".overlay").addEventListener("click", () => {
   modalEl.classList.remove("active");
 });
 
-
 function citySelectorclose() {
   citySelectorContainer.style.display = "none";
 }
 
-const menu = document.querySelector('citySelector-container');
-const arrow = document.getElementById('arrow');
-const items = document.querySelectorAll('li');
-const container = document.getElementById('citySelector');
+const menu = document.querySelector("citySelector-container");
+const arrow = document.getElementById("arrow");
+const items = document.querySelectorAll("li");
+const container = document.getElementById("citySelector");
 let currentItem = 0;
-document.addEventListener('keydown', function (event) {
+document.addEventListener("keydown", function (event) {
   event.preventDefault();
   switch (event.key) {
-    case 'ArrowDown':
+    case "ArrowDown":
       if (arrow.offsetTop + arrow.offsetHeight < container.offsetHeight) {
         if (currentItem < items.length - 1) {
           currentItem++;
-          arrow.style.top = items[currentItem].offsetTop + 'px';
+          arrow.style.top = items[currentItem].offsetTop + "px";
         }
       } else {
         if (currentItem < items.length - 1) {
           currentItem++;
-          arrow.style.top = items[currentItem].offsetTop + 'px';
+          arrow.style.top = items[currentItem].offsetTop + "px";
           container.scrollTop = items[currentItem].offsetTop;
         } else if (currentItem == items.length - 1) {
-          currentItem = 0
+          currentItem = 0;
           container.scrollTop = items[currentItem].offsetTop;
-          arrow.style.top = items[currentItem].offsetTop + 'px';
+          arrow.style.top = items[currentItem].offsetTop + "px";
         }
       }
       region = items[currentItem].textContent;
       break;
-    case 'ArrowUp':
+    case "ArrowUp":
       // move the arrow to the previous item
       if (currentItem == 0) {
         currentItem = items.length - 1;
-        arrow.style.top = items[currentItem].offsetTop + 'px';
-        container.scrollTop = items[currentItem].offsetTop
+        arrow.style.top = items[currentItem].offsetTop + "px";
+        container.scrollTop = items[currentItem].offsetTop;
       } else if (currentItem > 0) {
         currentItem--;
-        arrow.style.top = items[currentItem].offsetTop + 'px';
-        container.scrollTop = items[currentItem].offsetTop
+        arrow.style.top = items[currentItem].offsetTop + "px";
+        container.scrollTop = items[currentItem].offsetTop;
       }
       region = items[currentItem].textContent;
       break;
 
-    case 'Enter':
-
+    case "Enter":
       if (citySelectorContainer.style.display === "none") {
-        return
-
+        return;
       } else {
         //region = items[currentItem].textContent;
         fetchWeather(region, day);
       }
       citySelectorclose();
   }
-
 });
 
-document.addEventListener('touchstart', function (event) {
+document.addEventListener("touchstart", function (event) {
   // get the position of the touch
   const touchY = event.target.offsetTop;
 
@@ -323,14 +378,13 @@ document.addEventListener('touchstart', function (event) {
   //arrow.style.top = closestItem.offsetTop + 'px';
 });
 
-
-items.forEach(element => {
+items.forEach((element) => {
   element.addEventListener("mouseover", mouseEnterEffect);
   element.addEventListener("mouseout", mouseMoveEffect);
-})
+});
 
 function mouseEnterEffect(e) {
-  arrow.style.top = this.offsetTop + 'px';
+  arrow.style.top = this.offsetTop + "px";
   region = this.textContent;
   arrow.style.backgroundColor = "rgba(0, 0, 0, 0)";
   this.style.backgroundColor = "rgba(0, 0, 0, 0.2)";
@@ -348,21 +402,21 @@ function mouseMoveEffect(e) {
 arrow.addEventListener("click", (e) => {
   region = arrow.classList[1];
   fetchWeather(region, day);
-})
+});
 
 const body = document.body;
 function checkElementType(e) {
   if (e.type === "keydown") {
-    items.forEach(element => {
+    items.forEach((element) => {
       element.removeEventListener("mouseover", mouseEnterEffect);
       element.removeEventListener("mouseout", mouseMoveEffect);
-    })
+    });
   } else {
-    items.forEach(element => {
+    items.forEach((element) => {
       element.addEventListener("mouseover", mouseEnterEffect);
       element.addEventListener("mouseout", mouseMoveEffect);
-    })
+    });
   }
 }
-body.addEventListener('keydown', checkElementType, false); //偵測按下按鍵的行為
-body.addEventListener('mouseover', checkElementType, false);
+body.addEventListener("keydown", checkElementType, false); //偵測按下按鍵的行為
+body.addEventListener("mouseover", checkElementType, false);
